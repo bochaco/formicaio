c68f3042c2b98bbb3d42f80853a2ce78eb7984ac
Gabriel Viganotti <gabrielviganotti@gmail.com>
2025-08-10 23:35:35 -0300

    refactor: use nom_openmetrics crate to parse node metrics

--- src/bg_tasks/metrics_client.rs
+++ src/bg_tasks/metrics_client.rs
@@ -5,6 +5,7 @@ use crate::{
 
 use alloy_primitives::U256;
 use chrono::Utc;
+use nom_openmetrics::parser::openmetrics;
 use std::{
     collections::HashMap,
     net::{IpAddr, Ipv4Addr},
@@ -37,6 +38,8 @@ const METRICS_PROXY_ADDR: &str = "METRICS_PROXY_ADDR";
 pub enum MetricsClientError {
     #[error(transparent)]
     TransportError(#[from] reqwest::Error),
+    #[error("Failed to parse node metrics data: {0}")]
+    ParseError(String),
 }
 
 // Client to query metrics from nodes
@@ -56,29 +59,23 @@ impl NodeMetricsClient {
 
     // Fetch, filter, and return the predefined type of metrics.
     pub async fn fetch_metrics(&self) -> Result<Vec<NodeMetric>, MetricsClientError> {
-        //logging::log!(
-        //    "Sending request to node metrics server: {} ...",
-        //    self.endpoint
-        //);
-
         let response = reqwest::get(&self.endpoint).await?.text().await?;
+        let (_remaining, families) = openmetrics(&response)
+            .map_err(|err| MetricsClientError::ParseError(err.to_string()))?;
 
         let mut fetched_metrics = Vec::new();
         let timestamp = Utc::now().timestamp_millis();
-        for line in response.lines() {
-            if line.starts_with('#') {
-                continue; // Skip comments
-            }
 
-            let parts: Vec<&str> = line.split_whitespace().collect();
-            if parts.len() >= 2 && NODE_METRICS_TO_COLLECT.contains(&parts[0]) {
-                let key = parts[0].to_string();
-                let value = parts[1].to_string();
-                fetched_metrics.push(NodeMetric {
-                    key,
-                    value,
-                    timestamp,
-                });
+        for family in families {
+            for sample in family.samples {
+                let key = sample.name();
+                if NODE_METRICS_TO_COLLECT.contains(&key) {
+                    fetched_metrics.push(NodeMetric {
+                        key: key.to_string(),
+                        value: sample.number().to_string(),
+                        timestamp,
+                    });
+                }
             }
         }
 
