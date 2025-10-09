b070ed0e2272896e9f7334221671e75a7cdb6241
Gabriel Viganotti <gabrielviganotti@gmail.com>
2025-10-09 01:02:10 -0300

    chore: upgrade cargo-leptos to v0.2.45

--- deploy/publish_new_formica.sh
+++ deploy/publish_new_formica.sh
@@ -1,1 +1,5 @@
+# Build and push the Formica image
 docker build -t bochaco/formica:latest --platform linux/amd64,linux/arm64 -f formica.Dockerfile --push --no-cache .
+
+# Build and push the Formicaio image
+docker build -t bochaco/formicaio:latest --build-arg BUILD_ARGS='--features native' --platform linux/amd64,linux/arm64 --push .
